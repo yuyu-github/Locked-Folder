a51f56be7b8f3446433de64a3691b509ebac15e8
yuyu <hcsd7b34@gmail.com>
2025-09-14 21:49:25 +0900

    戻る/進むを実装

--- src/renderer/manager.ts
+++ src/renderer/manager.ts
@@ -3,10 +3,30 @@ import { update } from './filelist.js';
 
 export let currentPath = '/';
 
-export function setCurrentPath(path: string) {
+export let backStack: string[] = [];
+export let forwardStack: string[] = [];
+
+export function setCurrentPath(path: string, stack = true) {
   if (!path.startsWith('/')) path = `/${path}`;
   if (!path.endsWith('/')) path += '/';
+
+  if (stack && currentPath !== path) {
+    backStack.push(currentPath);
+    forwardStack = [];
+  }
   currentPath = path;
   update();
   updateAddressbar();
 }
+
+document.getElementById('back')!.addEventListener('click', () => {
+  if (backStack.length == 0) return;
+  forwardStack.push(currentPath);
+  setCurrentPath(backStack.pop()!, false);
+});
+
+document.getElementById('forward')!.addEventListener('click', () => {
+  if (forwardStack.length == 0) return;
+  backStack.push(currentPath);
+  setCurrentPath(forwardStack.pop()!, false);
+});
